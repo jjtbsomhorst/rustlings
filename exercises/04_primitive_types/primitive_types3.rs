b24c69b7a029bf3f5517071d092c870618a7d6e1
jjtbsomhorst <j.somhorst@gmail.com>
2024-12-31 06:44:21 +0100

    primitive types

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,6 +1,7 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
     // let a = ???
+    let a: Vec<String> = (1..=101).map(|i| format!("String {}", i)).collect();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
